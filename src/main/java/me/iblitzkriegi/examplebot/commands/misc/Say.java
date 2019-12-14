946ca8509fa84e03a73ebd649862671cbc8daff8
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-14 17:41:45 -0500

    Finish say command

--- src/main/java/me/iblitzkriegi/examplebot/commands/misc/Say.java
+++ src/main/java/me/iblitzkriegi/examplebot/commands/misc/Say.java
@@ -4,7 +4,10 @@ import me.iblitzkriegi.jdacommands.annotations.CommandInfo;
 import me.iblitzkriegi.jdacommands.annotations.permissions.RequiredPermissions;
 import me.iblitzkriegi.jdacommands.utilities.Command;
 import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.Permission;
+
+import java.util.Arrays;
 
 @CommandInfo(
         name = "say",
@@ -15,7 +18,15 @@ import net.dv8tion.jda.api.Permission;
 @RequiredPermissions({Permission.ADMINISTRATOR})
 public class Say extends Command {
     @Override
-    public void execute(CommandEvent commandEvent, String[] strings) {
+    public void execute(CommandEvent commandEvent, String[] arguments) {
+        if (arguments.length == 0) {
+            commandEvent.reply("You must include something to say!");
+            return;
+        }
+
+        MessageBuilder builder = new MessageBuilder();
+        Arrays.stream(arguments).forEach(argument -> builder.append(argument + " "));
+        commandEvent.reply(builder.build());
 
     }
 }
