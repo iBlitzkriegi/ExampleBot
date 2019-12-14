1d80585493f9043e85ee6f4c68e6a51ce58fd9bf
iBlitzkriegi <iBlitzkriegi@users.noreply.github.com>
2019-12-14 17:36:59 -0500

    Setup basic say command format

--- src/main/java/me/iblitzkriegi/examplebot/commands/misc/Say.java
+++ src/main/java/me/iblitzkriegi/examplebot/commands/misc/Say.java
@@ -1,4 +1,18 @@
 package me.iblitzkriegi.examplebot.commands.misc;
 
-public class Say {
+import me.iblitzkriegi.jdacommands.annotations.CommandInfo;
+import me.iblitzkriegi.jdacommands.utilities.Command;
+import me.iblitzkriegi.jdacommands.utilities.wrappers.CommandEvent;
+
+@CommandInfo(
+        name = "say",
+        desc = "Make the bot respond with whatever you say",
+        usage = "say <message>",
+        aliases = "repeat"
+)
+public class Say extends Command {
+    @Override
+    public void execute(CommandEvent commandEvent, String[] strings) {
+        
+    }
 }
